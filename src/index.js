20069851567790b3adabf11a2522f9f6902705f7
Darren Gegantino <darrengegantino@gmail.com>
2018-12-19 01:25:01 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText() {
+    return "Click me!";
+}
+
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
